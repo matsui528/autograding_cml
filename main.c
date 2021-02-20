0f67641e49a620a2ab989a1181cfcf7d79969229
Yusuke Matsui <matsui528@gmail.com>
2021-02-20 15:06:10 +0900

    initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int multiply (int a, int b) {
+    // この関数内部を編集してください
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Error\n");
+        return -1;
+    }
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    printf("%d\n", multiply(a, b));
+    return 0;
+}
